c2485f29acaf347bcfdd9d70041c1c8b4e1068c2
Paul Glavich <glav@theglavs.com>
2013-08-02 08:35:46 +1000

    Added missing file. Ignored of all resharpers/bin files

---
+++ Glav.AngularJSDemo.Web/demoPages/ServicesFactories/anotherController.js
@@ -0,0 +1,17 @@
+﻿window.app.controller('anotherController',
+    function ($scope, servicesFactoriesFactory, servicesFactoriesService) {
+
+        $scope.callFactory = function() {
+            servicesFactoriesFactory.performService();
+        };
+        $scope.callService = function () {
+            servicesFactoriesService.performService();
+        };
+
+        $scope.factoryPropertyValue = function() {
+            return servicesFactoriesFactory.factoryProperty;
+        };
+        $scope.servicePropertyValue = function () {
+            return servicesFactoriesService.serviceProperty;
+        };
+    });
