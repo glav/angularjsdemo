ef76a288b78a0662f6893443ce69f40d38c1f985
Paul Glavich <glav@theglavs.com>
2013-08-17 14:22:07 +1000

    fixed keyboard nav directive to also use arrow keys

--- Glav.AngularJSDemo.Web/Scripts/directives/SlideKeyboardNavigationDirective.js
+++ Glav.AngularJSDemo.Web/Scripts/directives/SlideKeyboardNavigationDirective.js
@@ -5,11 +5,10 @@
             link: function (scope, element, attributes) {
                 var bodyEl = $("body");
                 bodyEl.on('keydown', function (event) {
-                    console.log(event);
-                    if (event.keyCode === 32 || event.keyCode === 8) {
+                    if (event.keyCode === 32 || event.keyCode === 8 || event.keyCode === 37 || event.keyCode === 39) {
                         event.stopPropagation();
                         var nextRoute = null;
-                        if (event.keyCode === 32) {
+                        if (event.keyCode === 32 || event.keyCode == 39) {
                             // Space bar -advance next slide
                             nextRoute = $("#next-slide").attr('href').replace(/#/gi, '');
                         } else {
